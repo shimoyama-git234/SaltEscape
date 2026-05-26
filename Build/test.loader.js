7342b972f19c21e6097c07e86a26ebd7c7eb1ebc
shimoyama-git234 <shimoyama.git@gmail.com>
2026-05-26 12:54:01 +0900

    a
    a

--- Build/test.loader.js
+++ Build/test.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 60774359,
+    wasmFileSize: 60790954,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
